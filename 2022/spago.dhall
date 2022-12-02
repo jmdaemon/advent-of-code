6af4c913e9bbf18488109317e24908661eb02804
Joseph Diza <josephm.diza@gmail.com>
2022-12-01 18:28:56 -0800

    Create first real solution

--- 2022/spago.dhall
+++ 2022/spago.dhall
@@ -4,16 +4,22 @@ You can edit this file as you like.
 -}
 { name = "aoc-2022"
 , dependencies =
-  [ "console"
+  [ "arrays"
+  , "console"
   , "effect"
   , "foldable-traversable"
   , "integers"
   , "lists"
+  , "maybe"
+  , "node-buffer"
+  , "node-fs"
+  , "node-fs-aff"
   , "numbers"
   , "prelude"
+  , "st"
   , "strings"
-  , "node-fs-aff"
   , "test-unit"
+  , "tuples"
   ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- 2022/src/d1.purs
+++ 2022/src/d1.purs
@@ -1,5 +1,6 @@
 module D1 where
 
+import Data.Array
 import Data.Int
 import Data.String
 import Data.String.Pattern
@@ -11,6 +12,7 @@ import Data.Foldable (sum)
 import Data.Maybe (Maybe(..))
 import Data.String (split)
 import Data.String.CodePoints (length)
+import Data.Tuple (fst)
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(..))
@@ -20,61 +22,26 @@ import Node.FS.Sync (readTextFile)
 -- Facts:
 -- * The amount of food that one elf carries is separated by newlines before and after the list of amounts
 
---newtype Food = Food (Array Calories)
---newtype Calories = Calories Int
---newtype Elf = Elf (Array Food)
---newtype Elves = Elves (Array Elf)
-
--- TODO
 -- * Add stricter typing with newtypes? If added, create methods to convert to primitives
 
--- Elves -> Elf -> Food -> Calories -> Calory -> Int
---type Calory = Int
---type Calories = Array Calory
-type Calories = Int
-type Food = Array Calories
-type Elf = Array Food
 type Elves = Array Elf
-
--- InputFile -> InputElfCalories -> InputCalories
-type InputFile = String
-
---type InputElves = Array Food 
-type InputElves = Array String
---type InputFoods = Array String
-type InputElf = Array String
-
---type InputFoods = Array InputCalories
-type InputFoods = Array String
-type InputCalories = String
---type InputCalories = Array String 
+type Elf = Array Food
+type Food = Array Calories
+type Calories = Int
 
 -- InputFile -> InputElfCalories -> InputCalories -> Calories -> Calory -> Array Calory
-
--- String -> Array String -> Array (Array String) -> Array (Array Int) -> Array Int -> Int
-
--- String facilities
+type InputFile = String
+type InputElves = Array String
+type InputElf = Array String
+type InputFoods = Array String
+type InputCalories = String
 
 strToInt :: String -> Int
-strToInt s = case fromString s of
-    Nothing -> 0
-    Just res -> res
-
---fromStringToInt s = case strToIntMaybe s of
-    --Nothing -> 0
-    --Just res -> res
-
-
---fromStringToInt s = let res = strToIntMaybe s
---fromStringToInt :: String -> Int
---fromStringToInt s = case strToIntMaybe s of
-    --Nothing -> 0
-    --Just res -> res
---strToInt s = (\x |
-              --Nothing -> 0
-              --Just a -> a -> 0) fromString s 
-    --Nothing -> ""
-    --Just res -> res
+strToInt s = do
+    let result = fromString s
+    case result of
+         Nothing -> 0
+         Just int -> int
 
 splitStr :: String -> String -> Array String
 splitStr str delim = split (Pattern delim) str
@@ -86,13 +53,17 @@ splitBlankLine :: String -> Array String
 splitBlankLine s = splitStr s "\n\n"
 
 -- Problem specific
---splitElves :: InputFile -> InputElves
+
+-- Impure
+input :: String
+input = "input/d1.txt"
+
+readToString :: String -> Effect String
+readToString file = readTextFile UTF8 file
+
+-- Pure
 splitElves :: InputFile -> InputElves
 splitElves conts = splitBlankLine conts
-
--- TODO 
---splitFoods :: InputElves -> Array InputFoods
---splitFoods elves = map splitBlankLine elves
 
 splitFoods :: InputElf -> Array InputFoods
 splitFoods elf = map splitNewLine elf
@@ -101,8 +72,8 @@ toCalories :: InputFoods -> Array Calories
 toCalories food = map strToInt food
 
 -- TODO Double map
-splitCalories :: Array InputFoods -> Array InputCalories
-splitCalories foods = fold foods
+--splitCalories :: Array InputFoods -> Array InputCalories
+--splitCalories foods = fold foods
 
 -- TODO: Split the string and read the string input into numbers
 --readFood :: String -> Food
@@ -111,9 +82,28 @@ splitCalories foods = fold foods
 sumFood :: Food -> Calories
 sumFood food = sum food
 
--- TODO: Read the entire file into the string
-readFile :: String -> Effect String
-readFile file = readTextFile UTF8 file
+sumElf :: Elf -> Food
+sumElf elf = map sumFood elf
+
+fromElvesToCalories :: InputFile -> Array Calories
+fromElvesToCalories conts = do
+    let elves = (splitElves conts)
+    let foods = splitFoods elves
+    map sum (map toCalories foods) -- Returns the array of all the food calories summed
+    
+highestCalories :: Array Calories -> Calories
+highestCalories foods = do
+    let highestMaybe = last (sort foods)
+    case highestMaybe of 
+         Nothing -> 0
+         Just highest -> highest
+
+    --let sorted = sort calories
+        --fst sorted
+    --let calories = map toCalories foods
+    --let sorted = sort calories
+        --fst sorted
+--fromElvesToCalories conts = (map toCalories (map splitFoods (splitElves conts)))
 
 -- TODO: Split the file string into an array of Strings containing the calories string for each elf
 
@@ -121,10 +111,9 @@ readFile file = readTextFile UTF8 file
 -- TODO: Collect this result, and find the highest one
 -- Note that we might need to also remember which elf it was in the list
 
-
 main :: Effect Unit
 main = do
-    let input = readFile "input/d1.txt"
+    let conts = readToString input
     --let lines = splitStr input "\n"
     log $ "Advent of Code Day #1"
 
