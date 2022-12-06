1509164116052d9eaa1b3df3897ce34f66b12315
Joseph Diza <josephm.diza@gmail.com>
2022-12-06 14:38:59 -0800

    Remove dead code

--- 2022/src/d2.purs
+++ 2022/src/d2.purs
@@ -5,54 +5,18 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
--- Generic Types
-type ScoreHandType = Int
---type MatchResult = Int
+data ScoreHandType = Rock | Paper | Scissors
+data MatchResult = Win | Draw | Loss
 
-data Rock = Rock ScoreHandType
-data Paper = Paper ScoreHandType
-data Scissors = Scissors ScoreHandType
+calcScoreHandType :: ScoreHandType -> Int
+calcScoreHandType (Rock) = 1
+calcScoreHandType (Paper) = 2
+calcScoreHandType (Scissors) = 3
 
---data Win = Win MatchResult
---data Draw = Draw MatchResult
---data Loss = Loss MatchResult
-
-data MatchResult a = Win a | Draw a | Loss a
-
---calcScoreHandType :: ScoreHandType -> Int
---calcScoreHandType (Rock r) = (ScoreHandType r)
-
--- Type class for scoring
-class CalcScore a where
-    calcScore :: a -> Int
-
-instance calcScoreRock :: CalcScore Rock where
-    calcScore _ = 1
-
-instance calcScorePaper :: CalcScore Paper where
-    calcScore _ = 2
-
-instance calcScoreScissors :: CalcScore Scissors where
-    calcScore _ = 3
-    
---mkDraw :: Int
---mkDraw = 3
-
---mkWin :: Int
---mkWin = 6
-
---mkLoss :: Int
---mkLoss = 0
-
---calcScoreMatch :: MatchResult -> Int
---calcScoreMatch (Draw d) = 3
---calcScoreMatch (Win w) = 6
---calcScoreMatch (Loss w) = 0
-
-calcScoreMatch :: ∀ a. MatchResult a -> Int
-calcScoreMatch (Draw d) = 3
-calcScoreMatch (Win w) = 6
-calcScoreMatch (Loss l) = 0
+calcScoreMatch :: MatchResult -> Int
+calcScoreMatch (Draw) = 3
+calcScoreMatch (Win) = 6
+calcScoreMatch (Loss) = 0
 
 test :: Effect Unit
 test = do log $ "Advent of Code Day #1"
