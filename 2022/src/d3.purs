ba7a70553b05bec3266d56cc449e4fb509b29edd
Joseph Diza <josephm.diza@gmail.com>
2022-12-06 16:21:57 -0800

    Add todo

--- 2022/src/d3.purs
+++ 2022/src/d3.purs
@@ -5,6 +5,35 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+--data Rucksack = Rucksack String Int
+--data Priority = Priority Int Char
+
+type Rucksack =
+    { contents :: String
+    , size :: Int
+    }
+
+--type Priority =
+    --{ priority :: Int 
+    --, item :: Char
+    --}
+
+type Priority = Int 
+
+-- TODO:
+-- Split Rucksack string in half, assign string number and int, and return tuple of Rucksacks
+-- Find common types/characters in both strings
+-- Convert common characters to Priority types
+-- Sum priorities 
+-- Map across all rucksacks
+
+-- Test Case
+-- vJrwpWtwJgWrhcsFMMfFFhFp
+-- vJrwpWtwJgWr, hcsFMMfFFhFp
+-- Common type: p
+-- Priority { 16, p } or 16
+-- Sum: 16 (only 1 ruckack)
+
 test :: Effect Unit
 test = do
     log $ "Advent of Code Day #3"
