b3fe305ac2a6fd5e0f640e493fbf7ca7eb68e9e1
Joseph Diza <josephm.diza@gmail.com>
2022-12-11 16:44:07 -0800

    Prune dead code

--- 2022/src/d2.purs
+++ 2022/src/d2.purs
@@ -3,7 +3,7 @@ module D2 where
 import Prelude
 
 import Common (intToStr, readToString, splitNewLine, splitWhitespace)
-import Data.Array (foldl, head, index, last)
+import Data.Array (index)
 import Data.Foldable (sum)
 import Data.Int (decimal, toStringAs)
 import Data.Maybe (Maybe(..))
@@ -67,49 +67,18 @@ calcScoreGuide (You _) (You _) = 0
 calcScoreGuide (Opponent opp) (You you) = calcScore (You you) (Opponent opp)
 calcScoreGuide (You you) (Opponent opp) = calcScore (Opponent opp) (You you)
 
--- TODO: Read the file, collcet all the points, log the final score
 input :: String
 input = "src/input/d2.txt"
-
--- Read file, split strings on new line, split strings on space,
--- convert string to hand types, assign and types to opponent, player
-
-splitGuide :: String -> Array String
-splitGuide s = splitNewLine s
-
-splitHandTypes :: String -> Array String
-splitHandTypes s = splitWhitespace s
-
--- Contents -> Guides -> Hand Strings -> Hand Type -> Opponent, Player -> Score
--- String ->  Array String -> Array (Array String)
-
-parseHandTypes :: String -> Array (Array String)
-parseHandTypes conts = map splitHandTypes (splitGuide conts)
 
 makeTuple :: String -> String -> (Tuple Player Player)
 makeTuple a b = Tuple (Opponent a) (You b)
 
 toPlayers :: Array String -> Maybe (Tuple Player Player)
---toPlayers array = Tuple (Opponent (index array 0)) (You (index array 1))
-
 toPlayers array = case (index array 0), (index array 1) of
     p1, p2 -> do
        p1' <- p1
        p2' <- p2
        pure $ Tuple (Opponent p1') (You p2')
-
-       --pure $ Tuple (Opponent p1) (You p2)
-       --pure $ Tuple (Opponent p1) (You p2)
-
---toPlayers array = do 
-    --p1 = Opponent (index array 0)
-    --p2 = You (1 array 0)
-    --opp <- p1
-    --you <- p2
-
-    --opp <- index array 0
-    --you <- index array 1
-    --pure $ Tuple (Opponent opp) (You you)
 
 unwrapPlayers :: Maybe (Tuple Player Player) -> Tuple Player Player
 unwrapPlayers tuple = case tuple of
@@ -118,7 +87,7 @@ unwrapPlayers tuple = case tuple of
 
 findTotalScore :: String -> Int
 findTotalScore conts = res where
-    hands = splitNewLine conts # map splitHandTypes
+    hands = splitNewLine conts # map splitWhitespace
     players = map toPlayers hands
     scores = map (\x ->
                let _players = unwrapPlayers x
@@ -126,42 +95,6 @@ findTotalScore conts = res where
                    you = snd _players
                    in calcScoreGuide opp you) players
     res = sum scores
-    --res = map calcScoreGuide players
-
--- Converts the hand type
---strHandToType :: Array String -> Tuple Hand Hand
---strHandToType :: Array String -> Tuple Player Player
---strHandToType hands = 
-    --let x = (head hands)
-        --y = (last hands)
-     --in bind2 makeTuple x y
-
-
-    --let x = (head hands)
-        --y = (last hands) ado
-        --do
-        --x' <- (head hands)
-        --y' <- (last hands)
-        --pure $ makeTuple x' y'
-
-        --let x = (head hands)
-            --y = (last hands)
-
-        --x' <- x
-        --y' <- y
-     --in
-        --pure $ (Tuple x' y')
-        --((head hands) (last hands)) >>= \x y -> Tuple x y
-        --(x' y') >>= \x -> (\y -> Tuple x y)
-        --(x' y') >>= \x -> (\y -> Tuple x y)
-        --bind2 (\x -> (\y -> Tuple x y)) x' y'
-        --bind2 (x y (\y -> Tuple x y)) x' y'
-        --makeTuple x' y'
-        --bind2 makeTuple x' y'
-
---calcTotalScore :: Array String -> Int
---calcTotalScore conts = map splitHandTypes (splitGuide conts)
---calcTotalScore hands = head hands 
 
 test :: Effect Unit
 test = do
@@ -171,5 +104,4 @@ test = do
     log $ "Test Case"
     log $ "Score Expect 8: Actual " <> toStringAs decimal (calcScoreGuide opp you)
     log $ "Input Case"
-    --readToString input >>= \str -> log $ "Total number of points is: " <> intToStr (str)
     readToString input >>= \conts -> log $ "Total number of points is: " <> intToStr (findTotalScore conts)
