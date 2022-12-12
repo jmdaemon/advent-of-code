8fc9aa59bdd7b3164c7e4ec979d952d0b49759fb
Joseph Diza <josephm.diza@gmail.com>
2022-12-12 04:19:22 -0800

    Remove dead code

--- 2022/src/d1.purs
+++ 2022/src/d1.purs
@@ -3,9 +3,8 @@ module D1 where
 import Prelude
 
 import Common (inputPath, intToStr, readToString, splitBlankLine, splitNewLine, strToInt)
-import Data.Array (last, length, sort, splitAt, unsafeIndex)
+import Data.Array (length, sort, splitAt, unsafeIndex)
 import Data.Foldable (sum)
-import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
 import Partial.Unsafe (unsafePartial)
@@ -20,32 +19,23 @@ calsPerElf :: String -> Array Calories
 calsPerElf conts = splitBlankLine conts # map splitNewLine # map toCalories # map sum
 
 findHighestCalories :: String -> Calories
-findHighestCalories conts = let descending = calsPerElf conts # sort
-                             in unsafePartial $ unsafeIndex descending (length descending)
-
---findHighestCalories conts = res
-    --where descending = calsPerElf conts # sort
-          --res = unsafePartial $ unsafeIndex descending (length descending)
-
---findHighestCalories conts = let highest = calsPerElf conts # sort # last in
-    --case highest of 
-         --Nothing -> 0
-         --Just cals -> cals
+findHighestCalories conts = let ascending = calsPerElf conts # sort
+                             in unsafePartial $ unsafeIndex ascending (length ascending - 1)
 
 -- Part II
 findTopThreeCalories :: String -> Calories
 findTopThreeCalories conts = total where
-    highest = calsPerElf conts # sort
-    top_three = (splitAt (length highest - 3) highest).after
+    ascending = calsPerElf conts # sort
+    top_three = (splitAt (length ascending - 3) ascending).after
     total = sum top_three
 
 test :: Effect Unit
 test = do
     log $ "Advent of Code Day #1"
-    let e1 = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
     log $ "Test Case"
+    let e1 = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
     log $ "The elf with the highest number of calories carried is: Expect 24000 Actual " <> intToStr (findHighestCalories e1)
     log $ "Input Case"
     let input = inputPath "d1.txt"
-    readToString input >>= \str -> log $ "The highest number of calories is " <> intToStr (findHighestCalories str)
-    readToString input >>= \str -> log $ "The sum of the calories of the top three elves is " <> intToStr (findTopThreeCalories str)
+    readToString input >>= \str -> log $ "Part I: The highest number of calories is " <> intToStr (findHighestCalories str) -- 68787
+    readToString input >>= \str -> log $ "Part II: The sum of the calories of the top three elves is " <> intToStr (findTopThreeCalories str) -- 198041
