99fc8ee2904d248866710750e843b3622c6add2f
Joseph Diza <josephm.diza@gmail.com>
2022-12-11 17:02:59 -0800

    Solve Part II of D1

--- 2022/src/d1.purs
+++ 2022/src/d1.purs
@@ -3,7 +3,7 @@ module D1 where
 import Prelude
 
 import Common (readToString, intToStr, strToInt, splitBlankLine, splitNewLine)
-import Data.Array (last, sort)
+import Data.Array (last, length, sort, splitAt)
 import Data.Foldable (sum)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -14,6 +14,7 @@ type Calories = Int
 input :: String
 input = "src/input/d1.txt"
 
+-- Part I
 toCalories :: Array String -> Array Calories
 toCalories = map strToInt
 
@@ -26,6 +27,15 @@ findHighestCalories conts = let highest = calsPerElf conts # sort # last in
          Nothing -> 0
          Just cals -> cals
 
+-- Part II
+findTopThreeCalories :: String -> Calories
+findTopThreeCalories conts = total where
+    highest = calsPerElf conts # sort
+    len = length highest
+    beg = len - 3
+    top_three = (splitAt beg highest).after
+    total = sum top_three
+
 test :: Effect Unit
 test = do
     let e1 = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
@@ -34,3 +44,4 @@ test = do
     log $ "The elf with the highest number of calories carried is: Expect 24000 Actual " <> intToStr (findHighestCalories e1)
     log $ "Input Case"
     readToString input >>= \str -> log $ "The highest number of calories is " <> intToStr (findHighestCalories str)
+    readToString input >>= \str -> log $ "The sum of the calories of the top three elves is " <> intToStr (findTopThreeCalories str)
