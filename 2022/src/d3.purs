7a1002b33bdcf1d950afbc584d0dda7e46ab601f
Joseph Diza <josephm.diza@gmail.com>
2022-12-12 14:49:32 -0800

    Remove dead code

--- 2022/src/d3.purs
+++ 2022/src/d3.purs
@@ -25,60 +25,19 @@ type Priority = Int
 -- Map across all rucksacks
 
 halveString :: String -> { after :: String, before :: String }
---halveString s = let l = length s in splitAt l s
 halveString s = splitAt (length s / 2) s
 
--- TODO: Find a common substring in both strings
--- Algorithm:
--- Lexicographically order both strings
--- For every character in s1:
---  For every character in s2:
---      If they match
---          Return a new substring containing the character
--- Alternative:
--- Lexicographically order both strings
--- Better Algorithm:
--- Create two sets containing the string characters
--- Start with an initial empty array
--- Foreach char in s1
---  Foreach char in s2
---      if s1 == s2
---          append s1 to the new array
-
 -- Helper Functions
+
+-- Set Ops
 strToSet :: String -> Set Char
 strToSet s = fromFoldable (toCharArray s)
---strToSet s = let set = singleton in foreach (toCharArray s) (\x -> insert x set)
---strToSet s = foreach (toCharArray s) (\x -> insert x)
---strToSet s = set
-    --where
-          --initial = singleton
-          --chars = toCharArray s
-          ----set = foreach chars (\x -> insert x initial)
-          --set = foreach chars (\x -> x)
-
---strToSet s =
-    --let set = singleton in foreach (toCharArray s) (\x -> insert x set)
-    --let set = singleton in foreach (toCharArray s) insert
---strToSet s = let set = singleton in foreachE s (insert set)
---strToSet s = let set = singleton in map (insert) s
-
---setToStr :: Set String -> String
---setToStr set = let s = DA.singleton in map DA.insert set
 
 setToStr :: Set Char -> String
---setToStr set = fromCharArray <<< fromMap <<< toUnfoldable
---setToStr set = DA.fromFoldable set # toUnfoldable # fromCharArray
 setToStr set = DA.fromFoldable set # fromCharArray
 
-findSubstr :: String -> String -> String
-findSubstr s1 s2 = matching
-    where
-          set1 = strToSet s1
-          set2 = strToSet s2
-          --matches = set1 intersection set2
-          matches = intersection set1 set2
-          matching = setToStr matches
+findMatching :: String -> String -> String
+findMatching s1 s2 = intersection (strToSet s1) (strToSet s2) # setToStr 
 
 -- TOOD: Find a way to map the characters easily from lowercase 1-26, uppercase 27-52
 -- Idea: Generate the lowercase, uppercase mappings?
@@ -107,10 +66,7 @@ test = do
     log $ "Test Case"
     let r1 = "vJrwpWtwJgWrhcsFMMfFFhFp"
         str = halveString r1
-        --str = splitAt 8 r1
-        --len = length r1 / 2
-        --str = splitAt len r1
         s1 = str.before
         s2 = str.after
     log $ "String Before: " <> s1 <> " String After: " <> s2
-    log $ "Matching Items: " <> (findSubstr s1 s2)
+    log $ "Matching Items: " <> (findMatching s1 s2)
